7a366d8c11dc52cf076c8721cebb61c76122f80a
-k <slowdive@me.com>
2018-10-10 10:18:17 -0400

    v2

--- innsmouth.py
+++ innsmouth.py
@@ -1,23 +1,61 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import sys
+import random
+import datetime
+from twisted.internet import task
+from twisted.internet import reactor
+from twython import Twython
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
-import tweepy, time, sys
+TIMEOUT = datetime.timedelta(days=13).seconds
+twitter = Twython("YOUR API KEY",
+                  "YOUR API SECRET",
+                  "YOUR ACCESS TOKEN",
+                  "YOUR ACCESS TOKEN SECRET")
 
-argfile = str(sys.argv[1])
 
-#enter the corresponding information from your Twitter application:
-CONSUMER_KEY = '1234abcd...'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = '1234abcd...'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '1234abcd...'#keep the quotes, replace this with your access token
-ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access token secret
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+def reservoir(iterator):
+    """Select item from iterator.
+    Reservoir algorithm from http://stackoverflow.com/a/3540315/250241/
+    """
+    select = next(iterator)
+    for num, item in enumerate(iterator):
+        if random.randrange(num + 2):
+            continue
+        select = item
+    return select
 
-filename=open(argfile,'r')
-f=filename.readlines()
-filename.close()
 
-for line in f:
-    api.update_status(line)
-    time.sleep(1123200)
+def get_line(file_name):
+    """Open file and select tweetable line."""
+    with open(file_name) as open_file:
+        while True: # Loop until an appropriate sentence is found
+            open_file.seek(0)  # reset file iterator to 0
+            line = reservoir(open_file).strip().replace("  ", " ")
+            if line[0].isupper() and 4 < len(line) < 140:
+                return line
+
+
+def tweet(sentence):
+    """Tweet sentence to Twitter."""
+    try:
+        sys.stdout.write("{} {}\n".format(len(sentence), sentence))
+        twitter.update_status(status=sentence)
+    except:
+        pass
+
+
+def do_tweet(file_name):
+    """Get line and tweet it"""
+    line = get_line(file_name)
+    tweet(line)
+
+
+if __name__ == '__main__':
+    file_name = str(sys.argv[1])
+    l = task.LoopingCall(do_tweet, file_name)
+    l.start(TIMEOUT)
+    reactor.run()
